79a2b652d87dea7fe79f9d08b493b1e546395a16
ChalenK <chalenk454@gmail.com>
2024-10-11 19:42:42 -0500

    Started Creation of my 'pokedex'
    created a test folder for my js and added my initial variables for three pokemon

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,18 @@
-alert('Hello World');
-let favoriteFood = 'Wings';
-document.write(favoriteFood);
+let number32Name = "Nidoran(male)";
+let number32Height = ".5m";
+let number32Weight = "9kg";
+let number32Type = "Poison";
+let number33Name = "Nidorino";
+let number33Height = ".9m";
+let number33Weight = "19.5kg";
+let number33Type = "Poison";
+let number34Name = "Nidoking";
+let number34Height = "1.4m";
+let number34Weight = "62kg";
+let number34Type = "Poison \\ Ground"; 
+let pokemonListArray = [
+    number32Name + ' ' + number32Height + ' ' + number32Weight + ' ' + number32Type,
+    number33Name + ' ' + number33Height + ' ' + number33Weight + ' ' + number33Type,
+    number34Name + ' ' + number34Height + ' ' + number34Weight + ' ' + number34Type,
+]
+document.write(pokemonListArray);
