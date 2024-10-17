8f65e8e56cde4ebecaf6da9317bf4f63dfb4443e
ChalenK <chalenk454@gmail.com>
2024-10-16 20:16:44 -0500

    Committing changes to be reviewed
    Struggling to isolate the weight variables in the array and have them make an exclamation if they meet a threshold

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,18 +1,23 @@
-let number32Name = "Nidoran(male)";
-let number32Height = ".5m";
+
+     let number32Name = "Nidoran(male)";
 let number32Weight = "9kg";
 let number32Type = "Poison";
+
 let number33Name = "Nidorino";
-let number33Height = ".9m";
 let number33Weight = "19.5kg";
 let number33Type = "Poison";
+
 let number34Name = "Nidoking";
-let number34Height = "1.4m";
 let number34Weight = "62kg";
 let number34Type = "Poison \\ Ground"; 
-let pokemonListArray = [
-    number32Name + ' ' + number32Height + ' ' + number32Weight + ' ' + number32Type,
-    number33Name + ' ' + number33Height + ' ' + number33Weight + ' ' + number33Type,
-    number34Name + ' ' + number34Height + ' ' + number34Weight + ' ' + number34Type,
+
+let pokemonList = [
+    number32Name + '  ' + number32Weight + '  ' + number32Type,
+    number33Name + '  ' + number33Weight + '  ' + number33Type,
+    number34Name + '  ' + number34Weight + '  ' + number34Type
 ]
-document.write(pokemonListArray);
+for (let i = 3; i >= pokemonList.length; i--){
+  if( i == 0){
+    document.write("<p>" + i + "</p>")
+  }
+}
---
+++ JS/test.js
@@ -0,0 +1,13 @@
+let pokedex = [
+  "Nidoran" + '  ' + 9 + '  ' + "Poison",
+  "Nidorino" + '  ' + 19.5 + '  ' + "Poison",
+  "Nidoking" + '  ' + 62 + '  ' + "Poison/Ground"
+];
+   for (let i = 0; i < pokedex.length; i++){
+     console.log(pokedex)
+     document.write(pokedex)
+   }
+   let i = weight;
+   if (w < 10){
+  console.log("WOW! Thats Huge!")
+}
