dffa4d018e3d25c07b8472c7ecb23f03dac08cde
ChalenK <chalenk454@gmail.com>
2024-10-18 19:46:51 -0500

    Updated JS files, "Test" file still has issues

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,23 +1,15 @@
 
-     let number32Name = "Nidoran(male)";
-let number32Weight = "9kg";
-let number32Type = "Poison";
 
-let number33Name = "Nidorino";
-let number33Weight = "19.5kg";
-let number33Type = "Poison";
-
-let number34Name = "Nidoking";
-let number34Weight = "62kg";
-let number34Type = "Poison \\ Ground"; 
-
-let pokemonList = [
-    number32Name + '  ' + number32Weight + '  ' + number32Type,
-    number33Name + '  ' + number33Weight + '  ' + number33Type,
-    number34Name + '  ' + number34Weight + '  ' + number34Type
-]
-for (let i = 3; i >= pokemonList.length; i--){
-  if( i == 0){
-    document.write("<p>" + i + "</p>")
-  }
+let pokedex = [
+  "Nidoran" + '  ' + 9 + '  ' + "Poison",
+  "Nidorino" + '  ' + 19.5 + '  ' + "Poison",
+  "Nidoking" + '  ' + 62 + '  ' + "Poison/Ground"
+];
+   for (let i = 0; i < pokedex.length; i++){
+     console.log(pokedex)
+     document.write(pokedex)
+   }
+   let i = weight;
+   if (w < 10){
+  console.log("WOW! Thats Huge!")
 }
--- JS/test.js
+++ JS/test.js
@@ -1,13 +1,26 @@
 let pokedex = [
-  "Nidoran" + '  ' + 9 + '  ' + "Poison",
-  "Nidorino" + '  ' + 19.5 + '  ' + "Poison",
-  "Nidoking" + '  ' + 62 + '  ' + "Poison/Ground"
+  {
+    name: "Nidoran",
+    weight: 9,
+    types: "Poison",
+  },
+  {
+    name: "Nidorino",
+    weight: 19.5,
+    types: "Poison",
+  },
+  {
+    name: "Nidoking",
+    weight: 62,
+    types: "Poison/Ground"
+  }  
 ];
-   for (let i = 0; i < pokedex.length; i++){
-     console.log(pokedex)
-     document.write(pokedex)
-   }
-   let i = weight;
-   if (w < 10){
-  console.log("WOW! Thats Huge!")
+for (let i = 0; i < pokedex.length; i++){
+  console.log(pokedex[i].name)
+  document.write(pokedex[i].name)
+  for (let w = 0; w < pokedex.length; w++){
+console.log(pokedex[w].weight)
+document.write(pokedex[i].name + '  ' + pokedex[w].weight + '  ' + "<br/>")
 }
+}
+/* Array repeats names and I have no Idea why*/
