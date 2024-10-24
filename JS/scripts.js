cc57a11e80f0f6ce1f5e9420bd429e11c84a1c7c
ChalenK <chalenk454@gmail.com>
2024-10-23 21:06:17 -0500

    Added IIFE, an ADD function, and new Pokemon

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,9 +1,35 @@
-let pokedex = [
-  { name: "Nidoran", height: 0.5, type: "Poison" },
-  { name: "Nidorino", height: 0.9, type: "Poison" },
-  { name: "Nidoking", height: 1.4, type: "Poison/Ground" }
-];
-pokedex.forEach(function(pokemon) {
-  console.log(pokemon.name + ' (height: ' + pokemon.height + ') - ' + pokemon.type);
-  document.write(pokemon.name + ' (height: ' + pokemon.height + ') - ' + pokemon.type + ', ')
-});
+let pokeDex = (function () {
+  let pokemonList = [
+   {
+     name: "Nidoran",
+     height: 0.5,
+     type: "Poison"
+   },
+   {
+    name: "Nidorino",
+    height: 0.9,
+    type: "Poison"
+   },
+   {
+     name: "Nidoking",
+     height: 1.4,
+     type: "Poison/Ground"
+   }
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+console.log(pokeDex.getAll())
+pokeDex.add({ name: 'Rhydon'});
+console.log(pokeDex.getAll());
