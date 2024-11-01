9cb3df98fa9c3f2e40aaf3bcc285185a70ebfa0b
ChalenK <chalenk454@gmail.com>
2024-11-01 18:44:48 -0500

    added an API, showDetails function, and loadDetails function
    API pulls all original 151 pokemon into boxes and logs the data in the console when clicked

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,24 +1,17 @@
 let pokeDex = (function () {
-  let pokemonList = [
-   {
-     name: "Nidoran",
-     height: 0.5,
-     type: "Poison"
-   },
-   {
-    name: "Nidorino",
-    height: 0.9,
-    type: "Poison"
-   },
-   {
-     name: "Nidoking",
-     height: 1.4,
-     type: "Poison/Ground"
-   }
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151'
 
   function add(pokemon) {
-    pokemonList.push(pokemon);
+    if (
+      typeof pokemon === "object" &&
+    "name" in pokemon /*&&
+    "detailsUrl" in pokemon*/
+    ){
+      pokemonList.push(pokemon);
+    }else{
+      console.log("pokemon is not correct");
+    }
   }
 
   function getAll() {
@@ -33,30 +26,63 @@ let pokeDex = (function () {
  button.classList.add('button')
  listItem.appendChild(button);
  pokemonList.appendChild(listItem)
- button.addEventListener('click', showDetails)
+ button.addEventListener('click', function(event){
+   showDetails(pokemon)
+ });
   }
   function showDetails(pokemon) {
     console.log(event.target)
   }
   
+ function loadList() {
+   return fetch(apiUrl).then(function (response) {
+     return response.json();
+   }).then(function (json){
+     json.results.forEach(function (item) {
+       let pokemon = {
+         name: item.name,
+         detailsUrl: item.url
+       };
+       add(pokemon);
+       console.log(pokemon);
+     });
+   }).catch(function (e) {
+     console.error(e);
+   })
+ }
   
-  
+    function loadDetails(item){
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+          return response.json();
+        }).then(function (details) {
+          item.imageUrl = details.sprites.front_default;
+          item.height = details.height;
+          item.types = details.types;
+        }).catch(function (e){
+          console.error(e);
+        });
+  }
+
+  function showDetails(item) {
+    pokeDex.loadDetails(item).then(function (){
+      console.log(item);
+    })
+  }
+
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
   }
 })();
 
-console.log(pokeDex.getAll())
-pokeDex.add(
-  { 
-  name: 'Rhydon',
-  height: 1.9,
-  type: "Rock/Ground"
+pokeDex.loadList().then(function(){
+  pokeDex.getAll().forEach(function (pokemon) {
+    pokeDex.addListItem(pokemon);
   });
-console.log(pokeDex.getAll());
-
-pokeDex.getAll().forEach(function (pokemon) {
- pokeDex.addListItem(pokemon);
-})
+});
+  
